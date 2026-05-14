9ac68dc1d1598dc1b6585a49f664a13167e7a178
Ali <aliazadigithub@gmail.com>
2026-05-14 18:42:57 +0330

    React Router Dom Installation + Folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { useRoutes } from "react-router-dom";
+const routes = [
+  { path: "/", element: <h1></h1> },
+  { path: "/Login", element: <h1></h1> },
+  { path: "/dashboard", element: <h1></h1> },
+];
 function App() {
+  const router = useRoutes(routes);
 
   return (
-
-    <div className="text-[72px] text-center">
-      Kaktoos
+    <div>
+      <div className="text-[72px] text-center">Kaktoos</div>
+      {router}
     </div>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
